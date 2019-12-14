d5f73a800e05aedab72a9131598856a97faf411c
Adeeb Siddiqui <adeeb.bd@gmail.com>
2019-12-14 00:41:55 -0600

    Added custom repository method

--- src/main/java/com/ad/app/dao/EmployeeRepository.java
+++ src/main/java/com/ad/app/dao/EmployeeRepository.java
@@ -4,4 +4,6 @@ import com.ad.app.domain.Employee;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EmployeeRepository extends CrudRepository<Employee, String> {
+
+    Employee findByLastName(String lastName);
 }
