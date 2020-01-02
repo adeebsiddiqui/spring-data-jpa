d6a20f731f0c82fadffb8418d1a37a5371142d71
Adeeb Siddiqui <adeeb.bd@gmail.com>
2020-01-02 16:44:17 -0600

    Added datasource configuration comments

--- src/main/java/com/ad/app/config/DataSourceConfiguration.java
+++ src/main/java/com/ad/app/config/DataSourceConfiguration.java
@@ -8,7 +8,7 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfiguration {
-
+    /** Configure Datasource either in application.yml OR as a Spring Configuration Bean*/
     @Bean
     public DataSource mysqlDataSource() {
         return DataSourceBuilder.create()
